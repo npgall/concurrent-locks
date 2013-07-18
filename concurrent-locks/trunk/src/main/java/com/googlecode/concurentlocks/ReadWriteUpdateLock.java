20f81b446c5986545f63e5724ff58ce7380f040c
niall <niall@npgall.com>
2013-07-18 22:20:09 +0000

    JavaDocs for ReadWriteUpdateLock and ReentrantReadWriteUpdateLock.

--- concurrent-locks/trunk/src/main/java/com/googlecode/concurentlocks/ReadWriteUpdateLock.java
+++ concurrent-locks/trunk/src/main/java/com/googlecode/concurentlocks/ReadWriteUpdateLock.java
@@ -4,6 +4,15 @@ import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReadWriteLock;
 
 /**
+ * A <tt>ReadWriteUpdateLock</tt> extends the JDK {@link ReadWriteLock}, providing an update lock as a third type of
+ * associated lock in addition to the lock for read-only operation and the lock for writing.
+ * <p/>
+ * The {@link #updateLock update lock} supports read-only operations and can coexist with multiple
+ * {@link #readLock read lock}s held simultaneously by other reader threads. However it may also be upgraded from
+ * its read-only status to a {@link #writeLock write lock}, and it may be downgraded again back to a read lock.
+ * <p/>
+ * See implementation {@link ReentrantReadWriteUpdateLock} for more details.
+ *
  * @author Niall Gallagher
  */
 public interface ReadWriteUpdateLock extends ReadWriteLock {
--- concurrent-locks/trunk/src/main/java/com/googlecode/concurentlocks/ReentrantReadWriteUpdateLock.java
+++ concurrent-locks/trunk/src/main/java/com/googlecode/concurentlocks/ReentrantReadWriteUpdateLock.java
@@ -4,6 +4,52 @@ import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.*;
 
 /**
+ * An implementation of {@link ReadWriteUpdateLock}, thus extending the functionality of the JDK
+ * {@link ReentrantReadWriteLock} with an <b>update lock</b> in addition to the read and write lock, supporting upgrade
+ * from read-only operation to writing status, and downgrade again.
+ *
+ * <h1>Background - JDK ReentrantReadWriteLock</h1>
+ * The <tt>ReentrantReadWriteLock</tt> in the JDK classifies threads as needing to <i>read-without-write</i>,
+ * <i>write-without-read</i>, or <i>write-before-read</i>. Threads can obtain the read lock, or the write lock, and if
+ * they have the write lock, they can downgrade it to a read lock. But the key limitation is that it does not support
+ * <i>read-before-write</i>: threads which hold a read lock cannot upgrade it to a write lock.
+ * <p/>
+ * Imagine a data structure, let's say a cache, where most requests are to read data, but also there is a maintenance
+ * thread responsible for periodically traversing the data structure to find and update stale entries. The maintenance
+ * thread thus would mostly read entries while traversing the data structure, and periodically it might encounter
+ * entries which need to be updated.
+ * <p/>
+ * If a classic ReentrantReadWriteLock is used to control access, the <i>write-before-read</i> support afforded by that lock
+ * would be <i>insufficient</i>, because if at any point the thread updated an entry and then downgraded its write lock to a
+ * read lock, it would not be able to upgrade it again to a write lock if it found other stale entries. So the
+ * maintenance thread would need to hold the write lock for its entire traversal of the data structure, preventing read
+ * access for that entire duration.
+ *
+ * <h1>ReentrantReadWriteUpdateLock Overview</h1>
+ * The third type of lock provided, an <i>update lock</i>, is like a <i>super read lock</i>. It behaves like a read lock, in that it
+ * allows read access to the thread which holds it, and it concurrently <i>"plays nice"</i> with other threads which hold
+ * regular read locks, allowing those threads concurrent read access.
+ * <p/>
+ * The key difference is that the update lock can be upgraded from its read-only status, to a write lock. Thus it
+ * supports <i>read-before-write</i> usage. Also the write lock can be downgraded again to an update lock, <i>write-before-read</i>
+ * usage. A restriction however is that, similar to the write lock, only one thread may acquire the update lock at a
+ * time.
+ * <p/>
+ * This is sufficient in situations like the example above, to allow read-mostly, write-occasionally threads to operate
+ * on the data structure without blocking access to read-only threads most of the time: upgrading to the write lock only
+ * for the short periods in which they need it, before downgrading to the update lock again. As such it can reduce the
+ * latency for read-only requests, and increase concurrency in applications which otherwise would use a read-write lock.
+ *
+ * <h2>Lock Acquisition Paths</h2>
+ * <table border="1">
+ * <tr><th>Lock type</th><th>Associated Permissions</th><th>Lock acquisition paths</th><th>Lock downgrade paths</th><th>Prevented with exception *</th></tr>
+ * <tr><td>Read</td><td>Read (shared)</td><td>None → Read<br/>Read → Read (reentrant)</td><td>Read → None</td><td>Read → Update<br/>Read → Write</td></tr>
+ * <tr><td>Update</td><td>Read (shared)</td><td>None → Update<br/>Update → Update (reentrant)<br/>Write → Update (reentrant)</td><td>Update → None</td><td>Update → Read</td></tr>
+ * <tr><td>Write</td><td>Read (exclusive)<br/>Write (exclusive)</td><td>None → Write<br/>Update → Write<br/>Write → Write (reentrant)</td><td>Write → Update<br/>Write → None</td><td>Write → Read</td></tr>
+ * </table>
+ * * An <tt>IllegalStateException</tt> will be thrown if a thread holding a regular read lock tries to acquire the
+ * update or write lock, or if a thread holding the update or write lock tries to acquire a regular read lock.
+ * 
  * @author Niall Gallagher
  */
 public class ReentrantReadWriteUpdateLock implements ReadWriteUpdateLock {
